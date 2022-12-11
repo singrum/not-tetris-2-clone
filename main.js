43de151afb016de1e974ec83eb656610c698c2cc
Kang Hyomin <103807515+singrum@users.noreply.github.com>
2022-12-11 11:09:13 +0000

    modify

--- main.js
+++ main.js
@@ -1,35 +1,37 @@
 // import * as Matter from "matter-js"
 
 // module aliases
-var Engine = Matter.Engine,
+let Engine = Matter.Engine,
     Render = Matter.Render,
     Runner = Matter.Runner,
     Bodies = Matter.Bodies,
     Composite = Matter.Composite;
 
 // create an engine
-var engine = Engine.create();
+let engine = Engine.create();
 
 // create a renderer
-var render = Render.create({
+let render = Render.create({
     element: document.body,
     engine: engine
 });
 
 
+const width = 800, height = 600;
 // create two boxes and a ground
-var boxA = Bodies.rectangle(400, 200, 80, 80);
-var boxB = Bodies.rectangle(450, 50, 80, 80);
-var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+let boxA = Bodies.rectangle(400, 200, 80, 80);
+let boxB = Bodies.rectangle(450, 50, 80, 80);
+let ground = Bodies.rectangle(400, 600, 810, 60, { isStatic: true });
+let wall = [Bodies.rectangle(5, 285, 10, 570, {isStatic : true}), Bodies.rectangle(800 - 5, 285, 10, 570, {isStatic : true})]
 
 // add all of the bodies to the world
-Composite.add(engine.world, [boxA, boxB, ground]);
+Composite.add(engine.world, [boxA, boxB, ground, ...wall]);
 
 // run the renderer
 Render.run(render);
 
 // create runner
-var runner = Runner.create();
+let runner = Runner.create();
 
 // run the engine
 Runner.run(runner, engine);
