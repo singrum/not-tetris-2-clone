c2df2c5129d83e41992bf71d19412735c36607e8
Kang Hyomin <103807515+singrum@users.noreply.github.com>
2022-12-17 13:41:58 +0000

    x

--- js/cutting.js
+++ js/cutting.js
@@ -1,4 +1,6 @@
 function main(){
+
+
     const Engine = Matter.Engine,
         Bodies = Matter.Bodies,
         Body = Matter.Body,
@@ -12,49 +14,97 @@ function main(){
         MouseConstraint = Matter.MouseConstraint,
         Render = Matter.Render,
         Runner = Matter.Runner;
-    const engine = Engine.create({gravity : {x : 0, y : 1, scale : 0.001}}), world = engine.world;
-    const window_w = window.innerWidth;
-    const window_h = window.innerHeight;
+
+    const engine = Engine.create({gravity : {x : 0, y : 1, scale : 0.001}}), 
+        world = engine.world;
+
     const render = Render.create({
         element: document.body,
         engine: engine,
         options: {
-        wireframes: false,
-        showInternalEdges: false,
-        width: window_w,
-        height: window_h,
-        background: "transparent"
+            wireframes: false,
+            showInternalEdges: false,
+            width: window.innerWidth,
+            height: window.innerHeight,
+            background: "transparent"
         }
     });
+    console.log(render)
+
+    const WindowProp = {width : window.innerWidth, height : window.innerHeight},
+        Floor = {height : 200},
+        Ceiling = {height : 100},
+        Space = {
+            ratio : 3/7, 
+            height : WindowProp.height - Floor.height - Ceiling.height,
+            y : Ceiling.height,
+            row : 21,
+        },
+        Unit = Space.height / Space.row;
+        Space.width = Space.height * Space.ratio;
+        Space.x = (WindowProp.width - Space.width) / 2;
+        Space.y = Ceiling.height;
+        Space.column = Space.row * Space.ratio;
 
 
-    const floor_h = 200;
-    const frameRatio = 3/7;
-    const frame_h = window_h - floor_h;
-    const frame_w = frameRatio * frame_h;
-    const frame_offset = {x : (window_w - frame_w)/2, y : floor_h};
-    const unit = frame_h / 21;
-    const unit_h = 21, unit_w = 9;
-    const offset = {x : (window_w - frame_w)/2, y : 100};
-    let ground = Bodies.rectangle(window_w/2, window_h - floor_h/2 , frame_w, floor_h, { isStatic: true, render : {fillStyle: "#000000", lineWidth: 0}});
-    ground.friction = 0.2;
-    ground.frictionStatic = 0.3;
-    let leftWall = Bodies.rectangle((window_w - frame_w)/4, window_h / 2, (window_w - frame_w)/2,window_h, { isStatic: true, render : {fillStyle: "#000000", lineWidth: 0}});
-    leftWall.friction = 0;
-    let rightWall = Bodies.rectangle(window_w - (window_w - frame_w)/4, window_h / 2,(window_w - frame_w)/2,window_h,{ isStatic: true, friction : 0, render : {fillStyle: "#000000", lineWidth: 0}});        leftWall.friction = 0;
-    rightWall.friction = 0;
 
-    Composite.add(world, ground)
-    Composite.add(world, leftWall)
-    Composite.add(world, rightWall)
 
-    // result = Bodies.fromVertices(0, 0, shapeOption.vertices, {
-    //     frictionAir : 0.1,
-    //     friction : 0.2,
-    //     frictionStatic : 0.3,
-    //     isStatic: static,
-    //     render : {fillStyle: shapeOption.color}
-    // })
+
+
+    let floor = Bodies.rectangle(WindowProp.width / 2, WindowProp.height - Floor.height / 2, Space.width, Floor.height, 
+        {
+            isStatic: 1, 
+            friction : 0.2, 
+            frictionStatic : 0.3, 
+            render : {fillStyle: "#000000", lineWidth: 0}
+        }
+    );
+
+    let ceiling = Bodies.rectangle(WindowProp.width / 2, Ceiling.height / 2, Space.width, Ceiling.height, 
+        {
+            isStatic: 1, 
+            friction : 0.2, 
+            frictionStatic : 0.3, 
+            render : {fillStyle: "#000000", lineWidth: 0}
+        }
+    );
+
+    let leftWall = Bodies.rectangle((WindowProp.width - Space.width)/4, WindowProp.height / 2, (WindowProp.width - Space.width)/2,WindowProp.height, 
+        {
+            isStatic: 1, 
+            friction : 0,
+            render : {fillStyle: "#000000", lineWidth: 0}
+        }
+    );
+            
+    let rightWall = Bodies.rectangle(WindowProp.width - (WindowProp.width - Space.width)/4, WindowProp.height / 2,(WindowProp.width - Space.width)/2,WindowProp.height,
+        {
+            isStatic: 1, 
+            friction : 0, 
+            render : {fillStyle: "#000000", lineWidth: 0}
+        }
+    );        
+    console.log(floor, leftWall, rightWall)
+    Composite.add(world, [floor, ceiling, leftWall, rightWall]);
+
+
+
+
+    Render.run(render);
+    let runner = Runner.create();
+    Runner.run(runner, engine);
+    let mouse = Mouse.create(render.canvas),
+    mouseConstraint = MouseConstraint.create(engine, {
+        mouse: mouse,
+        constraint: {
+            stiffness: 0.2,
+            render: {
+            visible: false
+            }
+        }
+    });
+    Composite.add(world, mouseConstraint);
+    render.mouse = mouse;
 }
 
 window.onload = main
